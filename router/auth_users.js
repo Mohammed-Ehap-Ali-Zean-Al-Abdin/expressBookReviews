9d1a5e75dc334734a16974febeab275f74218e27
Mohammed-Ehap-Ali-Zean-Al-Abdin <mohammed_ehap_zean@yahoo.com>
2023-11-16 13:10:12 +0200

    quick fix

--- router/auth_users.js
+++ router/auth_users.js
@@ -10,11 +10,7 @@ const isValid = (username) => {
   let userswithsamename = users.filter((user) => {
     return user.username === username;
   });
-  if (userswithsamename.length > 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return userswithsamename.length > 0;
 };
 
 const authenticatedUser = (username, password) => {
