0bb283a826522b331b4c66d2f52217d973b0d10f
Mohammed-Ehap-Ali-Zean-Al-Abdin <mohammed_ehap_zean@yahoo.com>
2023-11-29 13:23:07 +0200

    commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const cors = require("cors")
 const app = express();
-
+app.use(cors({origin:"*"}))
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
